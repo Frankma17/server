02e2ebf5ec62d242f5fc42de9376774009cc775e
Frankma17 <frankin.canon@uptc.edu.co>
2021-08-26 15:01:37 -0500

    Status Random

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-res.status(500).send('error saving');
-
 app.get('/', (req, res) => {
-  res.sendStatus(404);
-  res.send('Hello World!')
+  let error = 404;
+  let ok = 202;
+
+  let option = random(0, 1)
+
+  if (option === 1) {
+    res.status(202).send('ERROR 202');
+  } else {
+    res.status(500).send('ERROR 404');
+  }
 })
 
+function random(inferior, superior) {
+  var possibilities = superior - inferior;
+  var random = Math.random() * (possibilities + 1);
+  random = Math.floor(random);
+  return inferior + random;
+}
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Listening http://localhost:${port}`)
 })
