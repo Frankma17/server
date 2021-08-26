3ce4f7f0fe532b45fc217ee3a797645a70f41257
Frankma17 <frankin.canon@uptc.edu.co>
2021-08-25 23:10:43 -0500

    Test

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+res.status(500).send('error saving');
+
 app.get('/', (req, res) => {
   res.sendStatus(404);
   res.send('Hello World!')
